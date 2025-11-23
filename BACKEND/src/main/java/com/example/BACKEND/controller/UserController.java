25c0644af5375cfce88cd9faae681a5eeeee6fba
Rahul Rathod <rrathod7602@gmail.com>
2025-11-23 21:30:55 +0530

    fix: Update CORS configuration and database connection settings for production

--- BACKEND/src/main/java/com/example/BACKEND/controller/UserController.java
+++ BACKEND/src/main/java/com/example/BACKEND/controller/UserController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://number-system-conversion-one.vercel.app/")
 @RestController
 @RequestMapping("/users")
 public class UserController {
--- FRONTEND/src/api/api.js
+++ FRONTEND/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080", // your backend URL
+  baseURL: "http://localhost:9090", // your backend URL
 });
 
 export default api;
